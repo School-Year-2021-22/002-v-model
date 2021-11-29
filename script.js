6cab958a12fdeea59a3ce5d31cb2552ea08f50c4
Paolo Cargnin <paolo.cargnin@h-is.com>
2021-11-29 14:13:21 +0100

    apis

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const app = {
     data() {
         return {
-            name: 'Mario',
+            name: 'Luke',
             results: false
         }
     },
+    methods: {
+        async search() {
+            //
+            console.log(this.name)
+            const result = await axios.get('https://pokeapi.co/api/v2/pokemon/'+this.name)
+            this.results = result.data
+        }
+    },
     async mounted() {
-        const result = await axios.get('https://swapi.dev/api/people/?search=Luke&format=json')
-        this.results = result.data
+        // const result = await axios.get('https://swapi.dev/api/people/?search=Boba&format=json')
+        // this.results = result.data
     }
 }
 
